177a917c66fd84b63eda2060a45e83ead536892e
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-10-28 11:17:45 +0100

    ml: add explanation of 2 variants of residual path

--- ml/models/HierarchyNodeModel.py
+++ ml/models/HierarchyNodeModel.py
@@ -9,18 +9,25 @@ class ResidualBlock(nn.Module):
 
         self.bn1 = nn.BatchNorm2d(in_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(in_channels, out_channels,
-                               kernel_size=3, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=stride, padding=1
+        )
 
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels,
-                               kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
+        # Residual highway
+        # If we have stride > 1 in the main path (the one with activations and so on)
+        # or if the number of input channels is different from the number of output channels
+        # then the shape of the output will be different from the shape of the input at the end of this path
+        # because of that we need to also adjust the shape of the output of residual highway
+        # we do that by applying 1x1 convolution with matching output channels and stride
+        # we use 1x1 kernel because we are using this conv layer mostly to adjust shape and not to learn new features
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels,
-                          kernel_size=1, stride=stride)
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
             )
+        # if the shape is not changing we can just use identity mapping (the output is the same as the input)
         else:
             self.shortcut = nn.Identity()
 
@@ -45,29 +52,30 @@ class HierarchyNodeModel(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(input_channels, 16, kernel_size=3,
-                      stride=1, padding=1),  # Changed from 7 to 3
+            nn.Conv2d(
+                input_channels, 16, kernel_size=3, stride=1, padding=1
+            ),  # Changed from 7 to 3
             nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Dropout2d(0.1),
-            nn.MaxPool2d(kernel_size=2, stride=2)  # Changed pool size to 2
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Changed pool size to 2
         )  # Output: 50x50
 
         self.layer1 = nn.Sequential(
             ResidualBlock(16, 48, dropout_rate=0.1),
             ResidualBlock(48, 48, dropout_rate=0.1),
-            ResidualBlock(48, 48, dropout_rate=0.1)
+            ResidualBlock(48, 48, dropout_rate=0.1),
         )
 
         self.layer2 = nn.Sequential(
             ResidualBlock(48, 96, stride=2, dropout_rate=0.15),
             ResidualBlock(96, 96, dropout_rate=0.15),
-            ResidualBlock(96, 96, dropout_rate=0.15)
+            ResidualBlock(96, 96, dropout_rate=0.15),
         )
 
         self.layer3 = nn.Sequential(
             ResidualBlock(96, 192, stride=2, dropout_rate=0.2),
-            ResidualBlock(192, 192, dropout_rate=0.2)
+            ResidualBlock(192, 192, dropout_rate=0.2),
         )
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -79,7 +87,7 @@ class HierarchyNodeModel(nn.Module):
             nn.Linear(192, 96),
             nn.ReLU(inplace=True),
             nn.Dropout(0.3),
-            nn.Linear(96, num_classes)
+            nn.Linear(96, num_classes),
         )
 
         # Weight initialization
@@ -87,8 +95,7 @@ class HierarchyNodeModel(nn.Module):
 
     def _init_weights(self, m):
         if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(
-                m.weight, mode='fan_in', nonlinearity='relu')
+            nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
         elif isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
