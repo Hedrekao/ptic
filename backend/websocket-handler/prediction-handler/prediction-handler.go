7a194aaa2da2b77306f323aa0e9d3443a2af5461
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2024-10-29 06:40:53 +0100

    init predictions

--- backend/websocket-handler/prediction-handler/prediction-handler.go
+++ backend/websocket-handler/prediction-handler/prediction-handler.go
@@ -8,13 +8,29 @@ import (
 func HandlePrediction(ctx *types.ConnectionContext) error {
 	// Predict the files
 	for _, file := range ctx.FilesToPredict {
-		prediction, err := model.Predict(file, ctx)
+		err := predictFile(file, ctx)
 		if err != nil {
 			return err
 		}
-
 		// TODO: continue here
 	}
+
+	return nil
+}
+
+func predictFile(filePath string, ctx *types.ConnectionContext) error {
+	prediction, err := model.Predict(filePath, ctx)
+	if err != nil {
+		return err
+	}
+
+	switch ctx.SelectedMode {
+	case types.ModeClassification:
+	}
+
+	ctx.SelectedMode
+
+	ctx.PredictionFiles = append(ctx.PredictionFiles, prediction)
 
 	return nil
 }
