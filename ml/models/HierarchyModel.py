c46fefea944a61af1ee564bf23f3b9001ac91eb2
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-11-06 10:58:37 +0100

    ml: predict function for the hierarchy model
    Predicts all path for all leaf nodes in parallel

--- ml/models/HierarchyModel.py
+++ ml/models/HierarchyModel.py
@@ -2,10 +2,12 @@ import torch
 import json
 import os
 import numpy as np
+import torch.nn.functional as F
 from glob import glob
 from PIL import Image
 from ml.models import HierarchyNodeModel
-from ml.utils.constants import MODELS_REGISTRY_PATH
+from ml.scripts.preprocess import create_transform_pipeline
+from ml.utils.constants import DATA_DIR, MODELS_REGISTRY_PATH
 from ml.utils.hierarchy import Hierarchy
 
 
@@ -16,6 +18,12 @@ class HierachyModel:
         self.hierarchy = Hierarchy()
         self.hierarchy_mask = np.load(
             os.path.join(MODELS_REGISTRY_PATH, "hierarchy_mask.npy"))
+        self.config = json.load(
+            open(os.path.join(DATA_DIR, "config.json"), "r"))
+
+        self.transform_pipeline = create_transform_pipeline(
+            (self.config["min_size"], self.config["min_size"])
+        )
 
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
@@ -52,4 +60,44 @@ class HierachyModel:
             self.models[file_name] = model
 
     def predict(self, image: Image):
-        pass
+
+        tensor = self.transform_pipeline(image)
+        tensor = tensor.unsqueeze(0).to(self.device)
+
+        root_node = self.hierarchy.get_root_id()
+        preds = []
+
+        queue = [root_node]
+
+        while len(queue) > 0:
+
+            node = queue.pop(0)
+            model_metadata = self.metadata[node]
+
+            is_single_label = model_metadata["is_single_label"]
+
+            children = self.hierarchy.get_non_leaf_children(node)
+
+            if is_single_label:
+                preds.append([1.0])
+            else:
+                model = self.models[node]
+                model.eval()
+
+                with torch.no_grad():
+                    output = model(tensor)
+                    output = F.softmax(output, dim=-1).squeeze().cpu().numpy()
+
+                preds.append(output)
+
+            queue.extend(children)
+
+        all_probs = np.concatenate(preds)
+
+        log_probs = np.log(all_probs + 1e-10)
+
+        leaf_probs = np.exp(self.hierarchy_mask @ log_probs)
+
+        leaf_probs /= leaf_probs.sum()
+
+        return leaf_probs
