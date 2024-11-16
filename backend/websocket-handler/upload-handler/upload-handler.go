4a5a08f2fcec78ac1cc68833153427a1b5779dae
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-11-16 16:49:54 +0100

    backend: images being uploaded to azure using blob storage

--- backend/websocket-handler/upload-handler/upload-handler.go
+++ backend/websocket-handler/upload-handler/upload-handler.go
@@ -3,43 +3,55 @@ package uploadhandler
 import (
 	"backend/utils"
 	"backend/websocket-handler/types"
+	"context"
 	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 )
 
+// TODO: clean this constant (maybe as env)
 const SAVE_DIR = "./uploads"
 
-func HandleFileUpload(Data types.FileUploadData) error {
-	// Extract the directory from the file name
-	dirPath := filepath.Join(SAVE_DIR, filepath.Dir(Data.FileName))
+func HandleFileUpload(ctx *types.ConnectionContext, data types.FileUploadData) error {
 
-	// Ensure the directory exists
-	if err := utils.EnsureDirExists(dirPath); err != nil {
-		return fmt.Errorf("failed to create directory: %w", err)
-	}
-
-	// Create the full file path including directories
-	filePath := filepath.Join(SAVE_DIR, Data.FileName)
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
-	}
-
-	defer file.Close()
-
-	// Decode the base64 file data
-	data, err := base64.StdEncoding.DecodeString(Data.FileData)
+	// Decode the base64 file imageData
+	imageData, err := base64.StdEncoding.DecodeString(data.FileData)
 	if err != nil {
 		return fmt.Errorf("failed to decode base64: %w", err)
 	}
 
-	// Write the data to the file
-	if _, err := file.Write(data); err != nil {
-		return fmt.Errorf("failed to write to file: %w", err)
-	}
+	if os.Getenv("ENV") == "AZURE" {
+		blobClient := ctx.BlobClient
+		containerName := "images"
 
+		// TODO: change the context to ctx ones we make it as go context
+		blobClient.UploadBuffer(context.TODO(), containerName, data.FileName, imageData, nil)
+
+	} else {
+		// Extract the directory from the file name
+		dirPath := filepath.Join(SAVE_DIR, filepath.Dir(data.FileName))
+
+		// Ensure the directory exists
+		if err := utils.EnsureDirExists(dirPath); err != nil {
+			return fmt.Errorf("failed to create directory: %w", err)
+		}
+
+		// Create the full file path including directories
+		filePath := filepath.Join(SAVE_DIR, data.FileName)
+
+		file, err := os.Create(filePath)
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+
+		defer file.Close()
+
+		// Write the data to the file
+		if _, err := file.Write(imageData); err != nil {
+			return fmt.Errorf("failed to write to file: %w", err)
+		}
+
+	}
 	return nil
 }
