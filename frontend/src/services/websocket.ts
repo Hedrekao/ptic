98ac6e56651e639cd2c990fae301680c4b0ce9c7
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-11-19 14:24:41 +0100

    frontend: ws logs

--- frontend/src/services/websocket.ts
+++ frontend/src/services/websocket.ts
@@ -1,21 +1,19 @@
-import {WS_URL} from "app/services/utils";
+import { WS_URL } from 'app/services/utils'
 import { TSendEvent } from 'app/services/types.send'
 import { ERegisterEvent, TRegisterEvent } from 'app/services/types.register'
 
 export const socket = new WebSocket(WS_URL)
 
+socket.onopen = () => console.log('Connected')
+socket.onerror = (error) => console.error('WebSocket error:', error)
+socket.onclose = (event) => console.log('Connection closed:', event.code, event.reason)
 
 const REGISTERED_EVENTS: Record<ERegisterEvent, (payload: TRegisterEvent['data']) => void> = {
-  [ERegisterEvent.UPLOAD_PROGRESS]: () => {
-  },
-  [ERegisterEvent.MODE_SELECTED]: () => {
-  },
-  [ERegisterEvent.PREDICTION_PROGRESS]: () => {
-  },
-  [ERegisterEvent.PREDICTION_APPROVAL_REQUEST]: () => {
-  },
-  [ERegisterEvent.CSV_FILE]: () => {
-  },
+  [ERegisterEvent.UPLOAD_PROGRESS]: () => {},
+  [ERegisterEvent.MODE_SELECTED]: () => {},
+  [ERegisterEvent.PREDICTION_PROGRESS]: () => {},
+  [ERegisterEvent.PREDICTION_APPROVAL_REQUEST]: () => {},
+  [ERegisterEvent.CSV_FILE]: () => {},
 }
 
 export const register = (event: ERegisterEvent, callback: (payload: TRegisterEvent['data']) => void) => {
@@ -29,5 +27,6 @@ socket.onmessage = (event) => {
 }
 
 export const send = (event: TSendEvent) => {
-    socket.send(JSON.stringify(event))
+  socket.send(JSON.stringify(event))
 }
+
