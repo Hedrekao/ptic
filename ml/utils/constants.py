bcf248ece6395de34ebe514ce23fe985dbeb2ab1
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2024-12-12 12:57:03 +0100

    ml: when running in gc env persist model files to drive instead of using ephemeral space

--- ml/utils/constants.py
+++ ml/utils/constants.py
@@ -1,7 +1,11 @@
+import os
+
 DATA_DIR = "./ml/data"
 RAW_IMAGES_PATH = "./ml/data/raw_images"
 PROCESSED_IMAGES_PATH = "./ml/data/processed_images"
 HIERARCHY_FILE_PATH = "./ml/data/hierarchy.csv"
-MODELS_REGISTRY_PATH = "./ml/models_registry"
+
+MODELS_REGISTRY_PATH = "/content/drive/MyDrive/bach/models_registry" if os.getenv(
+    "TRAINING_ENV", "LOCAL") == "GOOGLE_COLAB" else "./ml/models_registry"
 
 LOG_DIR = "./ml/logs"
